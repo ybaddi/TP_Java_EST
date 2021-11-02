d6ed58b6912c9c64bd9a036a2850799582e60ae6
Youssef baddi <baddiyoussef85@gmail.com>
2021-11-02 15:07:26 +0100

    add TP1 exercice1

---
+++ TP1/TP1_Java_Ex1/src/Point.java
@@ -0,0 +1,38 @@
+public class Point {
+    private double abs;
+    private double ord;
+
+
+    public double getAbs() {
+        return abs;
+    }
+
+    public double getOrd() {
+        return ord;
+    }
+
+    public void setAbs(double abs) {
+        this.abs = abs;
+    }
+
+    public void setOrd(double ord) {
+        this.ord = ord;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "abs=" + abs +
+                ", ord=" + ord +
+                '}';
+    }
+
+
+    public static void main( String[] args){
+        Point p = new Point();
+        p.setAbs(12.56);
+        p.setOrd(13.69);
+        System.out.println(p);
+    }
+
+}
