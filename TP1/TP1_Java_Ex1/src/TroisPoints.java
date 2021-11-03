246a0045ad527df5a147e31714f91b316d340761
Youssef baddi <baddiyoussef85@gmail.com>
2021-11-03 11:05:30 +0100

    edit  troisPoint class

--- TP1/TP1_Java_Ex1/src/TroisPoints.java
+++ TP1/TP1_Java_Ex1/src/TroisPoints.java
@@ -38,8 +38,28 @@ public class TroisPoints {
         double ab = premier.calculeDistance(deuxieme);
         double ac = premier.calculeDistance(troisieme);
         double bc = deuxieme.calculeDistance(troisieme);
-        if(ab == ac+bc || bc == ac+ab || ac == ab+bc)
-            return true;
-        else return false;
+//        if(ab == ac+bc || bc == ac+ab || ac == ab+bc)
+//            return true;
+//        else return false;
+        return (ab == ac+bc || bc == ac+ab || ac == ab+bc) ;
+    }
+
+    public boolean estIsocele(){
+        double ab = premier.calculeDistance(deuxieme);
+        double ac = premier.calculeDistance(troisieme);
+        double bc = deuxieme.calculeDistance(troisieme);
+//        if(ab == ac || bc == ac || bc == ab)
+//            return true;
+//        else return false;
+        return (ab == ac || bc == ac || bc == ab) ;
+    }
+
+    public static void main( String[] args){
+        Point p1 = new Point(14,15);
+        Point p2 = new Point(15,16);
+        Point p3 = new Point(15,16);
+        TroisPoints tp = new TroisPoints(p1,p2,p3);
+        System.out.println(tp.estIsocele());
+        System.out.println(tp.sontAlignes());
     }
 }
